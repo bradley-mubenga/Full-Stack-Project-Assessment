272ef9a0a7303b7a787d6a91497e47f0ab149853
Bradley Mubenga <bradley@Bradleys-MacBook-Air.local>
2021-08-12 14:54:22 +0200

    Worked At Home

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 //Express Modules
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-//PORT
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
 //
 const crudRoutes = require('./crud');
 
+//
+app.use(express.json());
+
+app.use(cors());
+
 //CRUD Routes Midlleware
 app.use(crudRoutes)
+
+//PORT
+app.listen(port, () => console.log(`Listening on port ${port}`));
